6f89ec3c130c10fc65a77575d11b49a861923865
guillermo <guillermoraven.96@hotmail.com>
2019-09-10 14:43:42 -0400

    query modified

--- components/energyAndPDU.js
+++ components/energyAndPDU.js
@@ -49,7 +49,7 @@ define(['angular', 'require'], function (angular, require) {
                 .query()
                 .then(values => {
                     this.MDCIDs = values.sort();
-                    console.log(values);
+
                     if (!this.MDCIDs.includes(this.MDC)) {
                         if (this.MDCIDs.includes($stateParams.MDC)) {
                             this.MDC = $stateParams.MDC;
@@ -58,8 +58,8 @@ define(['angular', 'require'], function (angular, require) {
                         } else {
                             this.MDC = null;
                         }
-                        this.MDCChanged();
                     }
+                    this.MDCChanged();
 
                 });
         };
@@ -70,42 +70,78 @@ define(['angular', 'require'], function (angular, require) {
             $stateParams.MDC = this.MDC;
             $state.go('.', $stateParams, { location: 'replace', notify: false });
             this.outputs = {};
-
             maPoint
                 .buildQuery()
                 .eq('tags.MDCID', this.MDC)
                 .limit(1000)
                 .query()
                 .then((points) => {
-                    this.getOutputsNames();
-                    this.orderPoints(points);
+                    this.generalPoints(points);
+                    this.refreshOutputs(points);
                 });
 
         };
 
-        this.getOutputsNames = () => {
-            let queryBuilder1 = maDataPointTags.buildQuery('outputs');
-
-            queryBuilder1.eq('MDCID', this.MDC);
-
-    
-            return queryBuilder1
+        this.refreshOutputs = (points) => {
+            let queryBuilder = maDataPointTags.buildQuery('outputs');
+            queryBuilder.ne('outputs', null);
+            queryBuilder.eq('MDCID', this.MDC);
+            return queryBuilder
                 .query()
                 .then(values => {
                     this.outputs = values.sort();
-                    Console.log(this.MDC)
+                    
+                    if (!this.outputs.includes(this.output)) {
+                        if (this.outputs.includes($stateParams.output)) {
+                            this.output = $stateParams.output;
+                        } else if (this.outputs.length) {
+                            this.output = this.outputs[0];
+                        } else {
+                            this.output = null;
+                        }
+                    }
+                        this.outputChanged (points);
                 });
         };
 
-        this.orderPoints = (points) => {
+        this.outputChanged = () => {
+            $stateParams.output = this.output;
+            console.log(this.output)
+            $state.go('.', $stateParams, { location: 'replace', notify: false });
+            return maPoint
+            .buildQuery()
+            .eq('tags.outputs', this.output)
+            .limit(1000)
+            .query()
+            .then((points) => {
+                this.especificPoints(points);
+            });
+        };
 
+        this.generalPoints = (points) => {
             this.totalAveragePower = this.filterByName(points, 'total-power-1');
             this.totalEnergy = this.filterByName(points, 'total-energy-1');
+        };
+
+        this.especificPoints = (points) => {
+            this.volt = this.filterByOutput(points,this.output)
+            this.current = this.filterByOutput(points,this.output)
+            console.log(points)
+            console.log(this.output)
+            console.log(this.outputs)
+            console.log(this.volt)
+            console.log(this.current)
         };
 
         this.filterByName = (points, name) => {
             return points.filter(point => {
                 return point.name == name;
+            })[0];
+        };
+
+        this.filterByOutput = (points,output) => {
+            return points.filter(point => {
+                return  point.tags.outputs == output;
             })[0];
         };
 
